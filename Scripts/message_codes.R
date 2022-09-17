a636b774ea95a3264a7e1804b086b785474d4472
Enrico Schumann <es@enricoschumann.net>
2022-09-17 18:32:20 +0200

    Add list of message codes

---
+++ Scripts/message_codes.R
@@ -0,0 +1,49 @@
+u <- "https://interactivebrokers.github.io/tws-api/message_codes.html"
+raw <- readLines(u, encoding = "utf-8")
+
+rows <- paste(raw, collapse = "")
+rows <- regmatches(txt, gregexec("<tr>.*?</tr>", txt))
+
+messages <- rows[[1]][grep("<td>[0-9]+", rows[[1]])]
+cols <- strsplit(messages, "</td><td>")
+if (!all(lengths(cols) == 3))
+    stop("format has changed")
+
+messages <- do.call(rbind, cols)
+colnames(messages) <- c("Code", "TWS message", "Additional notes")
+
+
+
+## Clean codes/text
+j <- "Code"
+messages[, j] <- gsub("<tr><td>", "", messages[, j])
+messages[, j] <- trimws(messages[, j])
+
+j <- "TWS message"
+messages[, j] <- textutils::HTMLdecode(messages[, j])
+messages[, j] <- trimws(messages[, j])
+
+j <- "Additional notes"
+messages[, j] <- gsub("</td></tr>", "", messages[, j])
+messages[, j] <- gsub("<a[^>]+?>(.*?)</a>", "\\1", messages[, j])
+messages[, j] <- gsub("<p[^>]+?>(.*?)</p>", "\\1", messages[, j])
+messages[, j] <- textutils::HTMLdecode(messages[, j])
+messages[, j] <- trimws(messages[, j])
+
+message_codes <- as.data.frame(messages, check.names = FALSE)
+
+save(message_codes,
+     file = "~/Packages/IButils/data/message_codes.RData",
+     version = 2)
+
+library("orgutils")
+filename <- "~/Packages/IButils/data/message_codes.txt"
+
+cat("## -*- mode: org; -*-
+
+## The data in this file are auto-generated from
+##    https://interactivebrokers.github.io/tws-api/message_codes.html
+
+", file = filename, append = FALSE, sep = "")
+
+cat(toOrg(message_codes), file = filename, append = TRUE, sep = "\n")
