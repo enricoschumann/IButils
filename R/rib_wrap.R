eda5388a4feebefca4b94948773d8ba12684cc58
Enrico Schumann <es@enricoschumann.net>
2022-09-17 19:18:55 +0200

    Add "wrap0", to become default wrap for all functions

--- R/rib_wrap.R
+++ R/rib_wrap.R
@@ -1,3 +1,210 @@
+wrap0 <- R6::R6Class("IBWrap",
+
+  class=      FALSE,
+  cloneable=  FALSE,
+  lock_class= TRUE,
+
+  public= list(
+
+      Data= NULL,
+
+      initialize= function() self$Data <- new.env(),
+
+    # Callbacks
+    tickPrice= function(tickerId, field, price, size, attrib) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickSize= function(tickerId, field, size) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickOptionComputation= function(tickerId, tickType, tickAttrib, impliedVol, delta, optPrice, pvDividend, gamma, vega, theta, undPrice) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickGeneric= function(tickerId, tickType, value) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickString= function(tickerId, tickType, value) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickEFP= function(tickerId, tickType, basisPoints, formattedBasisPoints, totalDividends, holdDays, futureLastTradeDate, dividendImpact, dividendsToLastTradeDate) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    orderStatus= function(orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    openOrder= function(orderId, contract, order, orderstate) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    openOrderEnd= function() warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+#    connectionClosed= function() warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    updateAccountValue= function(key, val, currency, accountName) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    updatePortfolio= function(contract, position, marketPrice, marketValue, averageCost, unrealizedPNL, realizedPNL, accountName) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    updateAccountTime= function(timeStamp) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    accountDownloadEnd= function(accountName) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    nextValidId= function(orderId) {
+      self$Data$nextId <- orderId
+      cat("Next valid Order Id is ", orderId, "\n", sep = "")
+      invisible(NULL)
+    },
+
+    contractDetails= function(reqId, contractDetails) {
+      self$Data$contract <- contractDetails
+      invisible(NULL)
+      ## warning("default method for ", get("res", envir = parent.frame(1))$fname)
+  },
+
+    bondContractDetails= function(reqId, contractDetails) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+  contractDetailsEnd= function(reqId) {
+      ## browser()
+      ## warning("default method for ", get("res", envir = parent.frame(1))$fname)
+},
+
+    execDetails= function(reqId, contract, execution) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    execDetailsEnd= function(reqId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    error= function(id, errorCode, errorString, advancedOrderRejectJson) {
+      cat(format(errorCode, width = 6), "|  ", "\n")
+      ## warning("default method for ", get("res", envir = parent.frame(1))$fname)
+  },
+
+    updateMktDepth= function(id, position, operation, side, price, size) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    updateMktDepthL2= function(id, position, marketMaker, operation, side, price, size, isSmartDepth) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    updateNewsBulletin= function(msgId, msgType, newsMessage, originExch) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    managedAccounts= function(accountsList) {
+      cat("Access to the following accounts: \n")
+      acc <- paste(paste("  ", sort(unlist(strsplit(accountsList, ",")))),
+                   collapse = "\n")
+      cat(acc, "\n\n")
+      invisible(NULL)
+  },
+
+    receiveFA= function(faDataType, xml) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalData= function(reqId, bar) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    scannerParameters= function(xml) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    scannerData= function(reqId, rank, contractDetails, distance, benchmark, projection, legsStr) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    realtimeBar= function(reqId, time, open, high, low, close, volume, wap, count) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    currentTime= function(time) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    fundamentalData= function(reqId, data) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    deltaNeutralValidation= function(reqId, deltaNeutralContract) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickSnapshotEnd= function(reqId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    marketDataType= function(reqId, marketDataType) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    commissionReport= function(commissionReport) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    position= function(account, contract, position, avgCost) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    positionEnd= function() warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    accountSummary= function(reqId, account, tag, value, currency) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    accountSummaryEnd= function(reqId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    verifyMessageAPI= function(apiData) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    verifyCompleted= function(isSuccessful, errorText) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    displayGroupList= function(reqId, groups) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    displayGroupUpdated= function(reqId, contractInfo) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    verifyAndAuthMessageAPI= function(apiData, xyzChallange) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    verifyAndAuthCompleted= function(isSuccessful, errorText) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+#    connectAck= function() warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    positionMulti= function(reqId, account, modelCode, contract, position, avgCost) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    positionMultiEnd= function(reqId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    accountUpdateMulti= function(reqId, account, modelCode, key, value, currency) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    accountUpdateMultiEnd= function(reqId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    securityDefinitionOptionalParameter= function(reqId, exchange, underlyingConId, tradingClass, multiplier, expirations, strikes) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    securityDefinitionOptionalParameterEnd= function(reqId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    softDollarTiers= function(reqId, tiers) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    familyCodes= function(familyCodes) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    symbolSamples= function(reqId, contractDescriptions) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    mktDepthExchanges= function(depthMktDataDescriptions) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickNews= function(tickerId, timeStamp, providerCode, articleId, headline, extraData) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    smartComponents= function(reqId, theMap) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickReqParams= function(tickerId, minTick, bboExchange, snapshotPermissions) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    newsProviders= function(newsProviders) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    newsArticle= function(requestId, articleType, articleText) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalNews= function(requestId, time, providerCode, articleId, headline) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalNewsEnd= function(requestId, hasMore) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    headTimestamp= function(reqId, headTimestamp) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    histogramData= function(reqId, data) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalDataUpdate= function(reqId, bar) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    rerouteMktDataReq= function(reqId, conid, exchange) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    rerouteMktDepthReq= function(reqId, conid, exchange) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    marketRule= function(marketRuleId, priceIncrements) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    pnl= function(reqId, dailyPnL, unrealizedPnL, realizedPnL) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    pnlSingle= function(reqId, pos, dailyPnL, unrealizedPnL, realizedPnL, value) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalTicks= function(reqId, ticks, done) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalTicksBidAsk= function(reqId, ticks, done) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalTicksLast= function(reqId, ticks, done) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickByTickAllLast= function(reqId, tickType, time, price, size, attribs, exchange, specialConditions) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickByTickBidAsk= function(reqId, time, bidPrice, askPrice, bidSize, askSize, attribs) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    tickByTickMidPoint= function(reqId, time, midPoint) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    orderBound= function(orderId, apiClientId, apiOrderId) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    completedOrder= function(contract, order, orderState) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    completedOrdersEnd= function() warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    replaceFAEnd= function(reqId, text) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    wshMetaData= function(reqId, dataJson) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    wshEventData= function(reqId, dataJson) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    historicalSchedule= function(reqId, startDateTime, endDateTime, timeZone, sessions) warning("default method for ", get("res", envir = parent.frame(1))$fname),
+
+    userInfo= function(reqId, whiteBrandingId) warning("default method for ", get("res", envir = parent.frame(1))$fname)
+  )
+)
+
 IBWrapHistData <- R6::R6Class("IBWrapHistData",
 
   class=      FALSE,
